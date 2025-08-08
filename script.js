138efe9c8e5675300f05b80373801bb6d855718b
PROroblox1212 <alexischarlebois499@gmail.com>
2025-08-08 01:11:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+document.getElementById("goInstallPageBtn").onclick = function() {
+  // URL of your installer
+  const installerUrl = "";
+  
+  // Create a temporary link and trigger download
+  const a = document.createElement("a");
+  a.href = installerUrl;
+  a.download = ""; // triggers download instead of navigation
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  
+  alert("Le téléchargement de l'installateur va commencer !");
+}
